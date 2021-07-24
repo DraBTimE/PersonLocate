baf3a0832b9558b5125cbf070bc3b6477f8863b0
DraBTimE <63486891+DraBTimE@users.noreply.github.com>
2021-07-24 05:24:35 +0300

    Update server.lua

--- PersonLocate/server.lua
+++ PersonLocate/server.lua
@@ -4,7 +4,10 @@ TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
 RegisterNetEvent("HasPhone")
 AddEventHandler("HasPhone", function(data)
-        local sPlayer, xPlayer = ESX.GetPlayerFromId(source), ESX.GetPlayerFromId(data);
+        TriggerClientEvent('HavePhone',source,false)
+        local sPlayer = ESX.GetPlayerFromId(source);
+        TriggerClientEvent('IsPolice',source,sPlayer.job.name == 'police')
+        local xPlayer = ESX.GetPlayerFromId(data);
         local item = xPlayer.getInventoryItem("bread").count;
         TriggerClientEvent('HavePhone',source,item > 0)
         TriggerClientEvent('IsPolice',source,sPlayer.job.name == 'police')
