167e6e49b4136dd233219b6c4305ce0186748e36
DraBTimE <63486891+DraBTimE@users.noreply.github.com>
2021-07-24 05:09:05 +0300

    Update server.lua

--- PersonLocate/server.lua
+++ PersonLocate/server.lua
@@ -4,24 +4,9 @@ TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
 RegisterNetEvent("HasPhone")
 AddEventHandler("HasPhone", function(data)
-        local hasPhone = '0'
-        TriggerClientEvent('HavePhone',source,hasPhone)
-        local xPlayer = ESX.GetPlayerFromId(data)
-        local sPlayer = ESX.GetPlayerFromId(source)
-        TriggerClientEvent('IsPolice',source,isPolice)
-        if sPlayer.job.name == 'police' then
-            local isPolice = '1'
-            TriggerClientEvent('IsPolice',source,isPolice)
-        end
-        print(xPlayer.getInventoryItem('phone').count)
-        if sPlayer.job.name == 'police' and xPlayer.getInventoryItem('phone').count > 0 then -- Here you can change the job from police to want you want the job to be.
-            hasPhone = '1'
-		    TriggerClientEvent('HavePhone',source,hasPhone)
-            local PlayerPed = GetPlayerPed(data)
-            local targetCoords = GetEntityCoords(PlayerPed)
-            TriggerClientEvent('TargetCoords',source,targetCoords)
-        else
-            local hasPhone = '0'
-            TriggerClientEvent('HavePhone',source,hasPhone)
-        end
+        local sPlayer, xPlayer = ESX.GetPlayerFromId(source), ESX.GetPlayerFromId(data);
+        local item = xPlayer.getInventoryItem("bread").count;
+        TriggerClientEvent('HavePhone',source,item > 0)
+        TriggerClientEvent('IsPolice',source,sPlayer.job.name == 'police')
+        TriggerClientEvent('TargetCoords',source,GetEntityCoords(GetPlayerPed(data)))
 end)
