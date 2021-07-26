3ff04ce8517c718353bde9d29f9ae674ef89e659
DraBTimE <63486891+DraBTimE@users.noreply.github.com>
2021-07-26 03:26:39 +0300

    Update and rename __resource.lua to fxmanifest.lua

---
+++ PersonLocate/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+games { 'rdr3', 'gta5' }
+
+author 'DraBTimE'
+description 'PersonLocate'
+version '1.0.0'
+
+
+client_script('client.lua')
+
+server_script('server.lua')
+
+
+
